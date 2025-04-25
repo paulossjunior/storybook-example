0d4999214be3c543faf603c4a73836aa4ece9f81
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2025-04-24 22:05:37 -0300

    first commit

---
+++ storybook-static/assets/Button.stories-C_dskAmC.js
@@ -0,0 +1,26 @@
+import{f as z}from"./index-BDOojamX.js";import{_}from"./Button-DqY_qVFN.js";import"./vue.esm-bundler-DJJpJwIm.js";const C={title:"Example/Button",component:_,tags:["autodocs"],argTypes:{size:{control:"select",options:["small","medium","large"]},backgroundColor:{control:"color"}},args:{primary:!1,onClick:z()}},r={args:{primary:!0,label:"Button"}},a={args:{primary:!1,label:"Button"}},e={args:{label:"Button",size:"large"}},s={args:{label:"Button",size:"small"}},o={args:{primary:!1,label:"Button"}};var t,n,l;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Button'
+  }
+}`,...(l=(n=r.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};var c,m,p;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    primary: false,
+    label: 'Button'
+  }
+}`,...(p=(m=a.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var i,u,g;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    size: 'large'
+  }
+}`,...(g=(u=e.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var d,y,b;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    size: 'small'
+  }
+}`,...(b=(y=s.parameters)==null?void 0:y.docs)==null?void 0:b.source}}};var f,B,S;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    primary: false,
+    label: "Button"
+  }
+}`,...(S=(B=o.parameters)==null?void 0:B.docs)==null?void 0:S.source}}};const E=["Primary","Secondary","Large","Small","Teste"];export{e as Large,r as Primary,a as Secondary,s as Small,o as Teste,E as __namedExportsOrder,C as default};
